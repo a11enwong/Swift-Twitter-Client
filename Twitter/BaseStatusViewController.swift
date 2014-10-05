b2b14c30ea45b388655a10ec39a30dfb43265c0e
hector <hectorups@gmail.com>
2014-10-04 18:48:12 -0700

    add user header profile

--- Twitter/BaseStatusViewController.swift
+++ Twitter/BaseStatusViewController.swift
@@ -184,4 +184,6 @@ UITableViewDelegate, TweetTableViewCellDelegate {
         tableView.tableFooterView = spinner
     }
     
+
+    
 }
--- Twitter/ProfileViewController.swift
+++ Twitter/ProfileViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwifteriOS
 
-class ProfileViewController: BaseStatusViewController {
+class ProfileViewController: BaseStatusViewController, UIScrollViewDelegate {
     var user: TwitterUser?
     var profileHeaderView: ProfileHeaderView?
     let HEADER_HEIGHT: CGFloat = 160
@@ -25,8 +25,16 @@ class ProfileViewController: BaseStatusViewController {
     func setupHeaderView() {
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: HEADER_HEIGHT)
         profileHeaderView = ProfileHeaderView(frame: frame)
-        tableView.tableHeaderView = profileHeaderView!
         profileHeaderView?.show(user!)
+        self.view.addSubview(profileHeaderView!)
+        self.view.sendSubviewToBack(profileHeaderView!)
+
+        
+        let headerFrame = CGRect(x: 0, y: 0, width: view.frame.width, height: HEADER_HEIGHT)
+        let transparentView = UIView(frame: headerFrame)
+        transparentView.backgroundColor = UIColor.clearColor()
+        tableView.tableHeaderView = transparentView
+        tableView.backgroundColor = UIColor.clearColor()
     }
     
     //getStatusesHomeTimelineWithCount
@@ -34,5 +42,18 @@ class ProfileViewController: BaseStatusViewController {
         includeEntities: Bool?, success: ((statuses: [JSONValue]?) -> Void)?, failure: Swifter.FailureHandler?) {
             swifter.getStatusesUserTimelineWithUserID(String(user!.userId!), count: count, sinceID: sinceID, maxID: maxID, trimUser: trimUser,
                 contributorDetails: contributorDetails, includeEntities: includeEntities, success: success, failure: failure)
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        println("view scrolled \(tableView.contentOffset.y)")
+        
+        let offset = tableView.contentOffset.y
+        
+        if offset < 0 {
+            profileHeaderView?.frame.size.height = HEADER_HEIGHT + (-1 * offset)
+            profileHeaderView?.center.y = (profileHeaderView!.frame.size.height / 2)
+        } else if offset > 0 {
+            profileHeaderView?.center.y = (HEADER_HEIGHT / 2) - offset
+        }
     }
 }
